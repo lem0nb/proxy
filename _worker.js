b0f0b1afe36f04fb9b993a623c516f0a63a49cf2
lem0nb <68461894+lem0nb@users.noreply.github.com>
2022-06-26 17:59:53 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'auto.c3pool.org:19999'
+      url.hostname = 'auto.c3pool.org'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
